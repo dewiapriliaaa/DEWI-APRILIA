1e17e686ad503532b1e87398de3ce70c48e67c22
WINDOWS-BJ66648\User <dewiaaaaap23@gmail.com>
2022-06-23 14:28:12 +0700

    memulai projek

---
+++ Test.py
@@ -0,0 +1,16 @@
+# > lebih dari
+# < kurang dari 
+# >= lebih dari sama dengan
+# <= kurang dari sama dengan 
+# = sama dengan 
+# != tidak sama dengan
+
+# semua hasil diatas akan menghasilkan boolean (true / false)
+
+print (5 < 7)
+print (10>=11)
+#print('10 > 11 =',a )
+
+#khusus ==dan != dapat digunakan untuk membandingkan string 
+print ("budi" =="budi")
+print ("budi"!="budi")
